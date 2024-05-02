858b6c2128472c6ebfbf83eb6761a16b8e480010
TheRednax <milanderuytter@gmail.com>
2024-05-02 09:15:11 +0200

    player.Cs update 2

--- Artic Anxiety/Scripts/Player.cs
+++ Artic Anxiety/Scripts/Player.cs
@@ -32,14 +32,39 @@ public partial class Player : CharacterBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		if(Input.IsActionJustPressed("ui_cancel"))
-		{
-		Input.SetMouseMode(Input.MouseMode.Captured);
-		}
+		//if(Input.IsActionJustPressed("ui_cancel"))
+		//{
+		//Input.SetMouseMode(Input.MouseMode.Captured);
+		//}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	//public override void _Input(InputEvent @event)
+	//{
+	//	base._Input(@event);
+	//	if(@event is InputEventMouseMotion motionEvent)
+	//	{
+	//		Vector3 rotDeg = RotationDegrees;
+	//		rotDeg.y -= motionEvent.Relative.x * mouse_sensitivity;
+	//	}
+	//}
+	public void _PhysicsProcess(float delta)
 	{
+		base._PhysicsProcess(delta);
+		handle_movement(delta);
+	}
+	private async void handle_movement(float delta)
+	{
+		Vector3 direction = new Vector3(Vector3.Zero);
+		if(Input.IsActionPressed("move_forward"))
+		{direction -= Transform.basis.z;}
+		if(Input.IsActionPressed("move_backward"))
+		{direction += Transform.basis.z;}
+		if(Input.IsActionPressed("move_left"))
+		{direction -= Transform.basis.X;}
+		if(Input.IsActionPressed("move_right"))
+		{direction += Transform.basis.X;}
+		
+		direction = direction.Normalized();
 	}
 }
