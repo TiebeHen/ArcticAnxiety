b9b1277b42ff49d983856b95626429d5316fd578
turbello <jonathan.bollaert@student.vives.be>
2024-05-03 16:30:13 +0200

    Snowball fixen

--- Artic Anxiety/Scripts/NewAnimationTest.gd
+++ Artic Anxiety/Scripts/NewAnimationTest.gd
@@ -60,10 +60,14 @@ func _physics_process(delta):
 			
 			
 			
-	if Input.is_action_pressed("click_throw"):
+	if Input.is_action_just_pressed("click_throw"):
 		print("gooi sneeuwbal")
 		var snowball_instance = SnowballScene.instantiate()
-		snowball_instance.position = Vector3(position.x, 1.5, position.z)
+		
+		var snowballPos = Vector3(0, 1.5, 0)
+		#snowball_instance.position  = snowballPos
+		snowball_instance.rotation  = rotation;
+		#print(snowballPos)
 		add_child_deferred(snowball_instance)
 		
 			
--- Artic Anxiety/Scripts/Snowball.cs
+++ Artic Anxiety/Scripts/Snowball.cs
@@ -3,22 +3,22 @@ using System;
 
 public partial class Snowball : Node3D
 {
-	public const float speed = 500.0f;
+	public const float speed = 25.0f;	
+	private Vector3 direction = new Vector3();
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//direction = new Vector3(1,0, 1).Rotated(Rotation);
+		//direction = new Vector3(0,1,0);
+		//Position = direction;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//Vector2 velocity = Velocity;
-		
-		//velocity = speed * direction;
-
-		//Velocity = velocity;
-		//MoveAndSlide();
+		Vector3 velocity = new Vector3(speed, 0, speed); // Set velocity towards bottom right
+		velocity = velocity.Normalized() * speed * (float)delta; // Scale velocity by delta time
+	
+		Translate(velocity); // Move the snowball
 	}
 }
