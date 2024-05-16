0c99d7f7459d5a45cd4b5b54529d3937bfd7a465
TheRednax <milanderuytter@gmail.com>
2024-05-16 16:00:47 +0200

    PUSH voor jonathan

--- Artic Anxiety/Scripts/DoodePenguin.gd
+++ Artic Anxiety/Scripts/DoodePenguin.gd
@@ -1,10 +1,12 @@
 extends Node
 
-#@onready var Dcam := $DoodePenguin/CameraDeath
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-#func set_deathcam():
-#	Dcam.current = true
-	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Defeat"):
+		$CameraDeath.current = true
