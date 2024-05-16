2cafbf7b6685f98a9a76482d53560a326df40bb7
TheRednax <milanderuytter@gmail.com>
2024-05-16 15:42:32 +0200

    upload voor Jonathan

---
+++ Artic Anxiety/Scripts/DoodePenguin.gd
@@ -0,0 +1,10 @@
+extends Node
+
+#@onready var Dcam := $DoodePenguin/CameraDeath
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#func set_deathcam():
+#	Dcam.current = true
+	
