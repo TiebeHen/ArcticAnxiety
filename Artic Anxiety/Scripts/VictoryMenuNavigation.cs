f7dcf41f1b74df11dd8ddf3a9a6525a49fbbdb8c
vicvandamme <vic.vandamme@student.vives.be>
2024-05-17 09:41:27 +0200

    bugs

--- Artic Anxiety/Scripts/VictoryMenuNavigation.cs
+++ Artic Anxiety/Scripts/VictoryMenuNavigation.cs
@@ -1,46 +1,47 @@
 using Godot;
 using System;
 
-public partial class MenuNavigation : Node
-{	
+public partial class VictoryMenuNavigation : Node
+{
+	
+	// HOME
+		private void _on_button_home_pressed()
+	{
+		Node newMenu = ResourceLoader.Load<PackedScene>("res://Scenes/Menus/StartMenu.tscn").Instantiate();
+		
+		GetParent().AddChild(newMenu);
+		QueueFree();
+	}
 
-	public void _on_button_home_mouse_exited()
+	private void _on_button_home_mouse_entered()
+	{
+		GetNode<Sprite2D>("Home/TextureHomeHover").Show();
+		GetNode<LineEdit>("Home/LineEditHomeHover").Show();
+	}
+
+	private void _on_button_home_mouse_exited()
 	{
 		GetNode<Sprite2D>("Home/TextureHomeHover").Hide();
 		GetNode<LineEdit>("Home/LineEditHomeHover").Hide();
 	}
-
-	public void _on_button_victory_exit_mouse_entered()
+	
+	// EXIT
+	
+	private void _on_button_exit_pressed()
+	{
+		GetTree().Quit();
+	}
+	
+	private void _on_button_exit_mouse_entered()
 	{
 		GetNode<Sprite2D>("Exit/TextureExitHover").Show();
 		GetNode<LineEdit>("Exit/LineEditExitHover").Show();
 	}
 
-	public void _on_button_victory_exit_mouse_exited()
+	private void _on_button_exit_mouse_exited()
 	{
 		GetNode<Sprite2D>("Exit/TextureExitHover").Hide();
 		GetNode<LineEdit>("Exit/LineEditExitHover").Hide();
 	}
 
-	public void _on_button_victory_exit_pressed()
-	{
-		GetTree().Quit();
-	}
-	
-	public void _on_button_home_pressed()
-	{
-		Node newMenu = ResourceLoader.Load<PackedScene>("res://Scenes/Menus/StartMenu.tscn").Instantiate();
-		
-		GetParent().AddChild(newMenu);
-		QueueFree();		
-	}
-	
-	public void _on_button_home_mouse_entered()
-	{
-		GD.Print("ff");
-	}
 }
-
-
-
-
