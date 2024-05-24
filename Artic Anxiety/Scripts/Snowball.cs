a6efb4b09c95c32433f4b93d9b67056a25ca79b3
TheRednax <milanderuytter@gmail.com>
2024-05-24 11:35:04 +0200

    snoball and icetile reskinning

--- Artic Anxiety/Scripts/Snowball.cs
+++ Artic Anxiety/Scripts/Snowball.cs
@@ -4,13 +4,18 @@ using System;
 
 public partial class Snowball : Node3D
 {
+	private double  maxTimeSnowball = 1;
+	private double timeLeftSnowball;
+	
+	
 	public const float speed = 25.0f;
 	//private Vector3 direction = new Vector3();
-	private	Vector3 _velocity = new Vector3(speed, 0, speed); // Set velocity towards bottom right
+	private	Vector3 _velocity = new Vector3(speed, speed, speed); // Set velocity towards bottom right
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		timeLeftSnowball = maxTimeSnowball;
 		//GD.Print("Hi");
 		//direction = new Vector3(0,1,0);
 		//Position = direction;
@@ -23,11 +28,13 @@ public partial class Snowball : Node3D
 	
 		Translate(_velocity); // Move the snowball
 		
+		timeLeftSnowball -= delta;
+		if(timeLeftSnowball < 0)
+		{
+			QueueFree();
+		}
 		
-		//if(position.x < -10 && position.x > 110 && position.x < -10 && position.x > 70)
-		//{
-			//queue_free();
-		//}
+		
 	}
 	
 	public void SetVelocity(Vector3 velocity)
