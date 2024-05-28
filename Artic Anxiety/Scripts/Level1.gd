bae1f7a2d1fe15e74dc9194045d78cfb01b3445e
turbello <jonathan.bollaert@student.vives.be>
2024-05-28 16:58:38 +0200

    Working LAN

--- Artic Anxiety/Scripts/Level1.gd
+++ Artic Anxiety/Scripts/Level1.gd
@@ -21,7 +21,6 @@ var BrokenIceTile = preload("res://Scenes/Game/Tiles/Level/BrokenIceTile.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_map()
-	set_process(true)
 
 func add_child_deferred(node):
 	call_deferred("add_child", node)
@@ -40,7 +39,7 @@ func load_map():
 				var tile = FullIceTile.instantiate() as Node3D
 				tile.position = Vector3(x, 0, z)
 				add_child_deferred(tile)
-				tiles.append(ClassTile.new(id, tile.position, tile))
+				tiles.append(ClassTile.new(ID_FULL_ICE_TILE, tile.position, tile))
 			else:
 				tiles.append(ClassTile.new(ID_WATER_TILE, Vector3(x, 0, z)))
 			z += 2
@@ -48,110 +47,122 @@ func load_map():
 		x += 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if GameManager.IsThisAServer == true:
 		if GameManager.GamePaused == true:
 			return
-		
-		if start_gameplay == true:
-			var newInstances = [] # Create a new list to hold instances to be added
-
-			for t in tiles: # Iterate over a copy of instances to avoid modification during enumeration
-				if t.get_id() == ID_WATER_TILE:
-					if randi() % 100000 < 3: # Adjust probability for adding ice
-						var instance = FullIceTile.instantiate() as Node3D
-						instance.position = t.get_position()
-						t.set_instance(instance)
-						t.set_id(ID_FULL_ICE_TILE) # Add new instance to the temporary list
-						add_child(instance)
-						# Call RPC to create ice on other peers
-						rpc("rpc_create_ice", t.get_position())
-
-			# Add new instances to the main collection after the loop
-			for newInstance in newInstances:
-				tiles.append(newInstance)
-			
-			BreakTiles(delta)
+	
+		for tile in tiles:
+			if tile.get_id() == ID_WATER_TILE:
+				if randf() < 0.00005: # Adjust probability for adding ice
+					check_to_create_ice(tile)
+					
+			if tile.get_id() == ID_FULL_ICE_TILE:
+				if randf() < 0.00002: # Adjust probability for breaking ice
+					check_to_break_ice(tile)
+					
+			if tile.get_id() == ID_BROKEN_ICE_TILE:
+				if randf() < 0.0005: # Adjust probability for deleting ice
+					check_to_delete_ice(tile)
 	else:
 		pass
-
-func startGame():
-	start_gameplay = true
-
+		
+func check_to_create_ice(tile: ClassTile):
+	var instance = FullIceTile.instantiate() as Node3D
+	instance.position = tile.get_position()
+	tile.set_instance(instance)
+	tile.set_id(ID_FULL_ICE_TILE)
+	add_child(instance)
+	# Call RPC to create ice on other peers
+	rpc("rpc_create_ice", tile.get_position())
+	
 @rpc("any_peer")
 func rpc_create_ice(_position: Vector3):
 	# This will be called on all peers
 	var instance = FullIceTile.instantiate() as Node3D
 	instance.position = _position
 	add_child(instance)
-	for t in tiles:
-		if t.get_position() == _position:
-			t.set_instance(instance)
-			t.set_id(ID_FULL_ICE_TILE)
+	for tile in tiles:
+		if tile.get_position() == _position:
+			tile.set_instance(instance)
+			tile.set_id(ID_FULL_ICE_TILE)
+	
+func check_to_break_ice(tile: ClassTile):
+	var parent = tile.get_instance().get_parent()
+	var children = tile.get_instance().get_children()
+	for child in children:
+		tile.get_instance().remove_child(child)
+	if tile.get_instance() != null:
+		parent.remove_child(tile.get_instance())
+		
+	var instance = BrokenIceTile.instantiate() as Node3D
+	instance.position = tile.get_position()
+	tile.set_instance(instance)
+	tile.set_id(ID_BROKEN_ICE_TILE)
+	add_child(instance)
+	# Call RPC to create ice on other peers
+	rpc("rpc_break_ice", tile.get_position())
 
 @rpc("any_peer")
-func rpc_delete_tile(_position: Vector3):
+func rpc_break_ice(_position: Vector3):
 	# This will be called on all peers
-	delete_tile_at_position(_position)
+	var instance = BrokenIceTile.instantiate() as Node3D
+	for tile in tiles:
+		if tile.get_position() == _position:
+			
+			var parent = tile.get_instance().get_parent()
+			var children = tile.get_instance().get_children()
+			for child in children:
+				tile.get_instance().remove_child(child)
+			if tile.get_instance() != null:
+				parent.remove_child(tile.get_instance())
+				
+			instance.position = tile.get_position()
+			tile.set_instance(instance)
+			tile.set_id(ID_BROKEN_ICE_TILE)
+			add_child(instance)
+
+func check_to_delete_ice(tile: ClassTile):
+	if tile != null and tile.get_instance() != null:
+		tile.get_instance().remove_child(tile.get_instance().get_children()[0])
+		
+	tile.set_id(ID_WATER_TILE)
+	# Call RPC to create ice on other peers
+	rpc("rpc_delete_ice", tile.get_position())
+				
+		
+@rpc("any_peer")
+func rpc_delete_ice(_position: Vector3):
+	# This will be called on all peers
+	for tile in tiles:
+		if tile.get_position() == _position:
+			tile.get_instance().remove_child(tile.get_instance().get_children()[0])
+			tile.set_id(ID_WATER_TILE)
+
+func startGame():
+	start_gameplay = true
 
 func delete_tile_with_radius(_position: Vector3, radius: int):
 	if radius == 5:
 		print("ok")
+
 	var playerX = _position.x
 	var playerZ = _position.z
+
 	for i in tiles:
 		var _pos = i.get_position()
 		if sqrt(pow(_pos.x - playerX, 2) + pow(_pos.z - playerZ, 2)) <= radius:
 			if i.get_id() != ID_WATER_TILE:
-				var parent = i.get_instance().get_parent()
-				var children = i.get_instance().get_children()
-				for child in children:
-					i.get_instance().remove_child(child)
-				if i.get_instance() != null:
-					parent.remove_child(i.get_instance())
+				var instance = i.get_instance()
+				var parent = instance.get_parent()
+				if parent != null:
+					var children = instance.get_children()
+					for child in children:
+						instance.remove_child(child)
+						parent.remove_child(instance)
+				
 				i.set_id(ID_WATER_TILE)
-				# Call RPC to delete tile on other peers
-				rpc("rpc_delete_tile", i.get_position())
-
-func delete_tile_at_position(_position: Vector3):
-	var playerX = _position.x
-	var playerZ = _position.z
-	for i in tiles:
-		var _pos = i.get_position()
-		if abs(_pos.x - playerX) <= 2 and abs(_pos.z - playerZ) <= 2:
-			if i.get_id() != ID_WATER_TILE:
-				var parent = i.get_instance().get_parent()
-				var children = i.get_instance().get_children()
-				for child in children:
-					i.get_instance().remove_child(child)
-				parent.remove_child(i.get_instance())
-				i.set_id(ID_WATER_TILE)
-				# Call RPC to delete tile on other peers
-				rpc("rpc_delete_tile", _pos)
-
-func BreakTiles(delta):
-	timeLeftTileBreaking -= delta
-	if timeLeftTileBreaking < 0:
-		for t in tiles:
-			if t.get_id() == ID_FULL_ICE_TILE:
-				if randi() % 300000 < 3: # Adjust probability for breaking ice
-					delete_tile_with_radius(t.get_position(), 1)
-					var instance = BrokenIceTile.instantiate() as Node3D
-					instance.position = t.get_position()
-					t.set_instance(instance)
-					t.set_id(ID_BROKEN_ICE_TILE)
-					add_child(instance)
-					# Call RPC to create broken ice on other peers
-					rpc("rpc_create_broken_ice", t.get_position())
-		timeLeftTileBreaking = maxTimeTileBreaking
-
-@rpc("any_peer")
-func rpc_create_broken_ice(_position: Vector3):
-	# This will be called on all peers
-	var instance = BrokenIceTile.instantiate() as Node3D
-	instance.position = _position
-	add_child(instance)
-	for t in tiles:
-		if t.get_position() == _position:
-			t.set_instance(instance)
-			t.set_id(ID_BROKEN_ICE_TILE)
+				
+				# Make sure the instance is part of the tree before calling rpc
+				if instance.is_inside_tree():
+					rpc("rpc_delete_ice", i.get_position())
