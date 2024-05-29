c3b0be91463f590558ecf2d58dea59352c052fd9
TheRednax <milanderuytter@gmail.com>
2024-05-29 18:31:01 +0200

    User Interface Changes

--- Artic Anxiety/Scripts/user_interface.gd
+++ Artic Anxiety/Scripts/user_interface.gd
@@ -4,6 +4,7 @@ static var SelectedAbility = 1
 @onready var rect_ability_1 = $AbilityAid/SelectedAbility/RectAbility1
 @onready var rect_ability_2 = $AbilityAid/SelectedAbility/RectAbility2
 @onready var rect_ability_3 = $AbilityAid/SelectedAbility/RectAbility3
+#all vars from the jezus "stamina" bar
 #timer to track jezusability
 var MaxtimeJezusAbility = 3.0
 var timerJezusAbility = MaxtimeJezusAbility
@@ -12,26 +13,38 @@ var JezusAbility_elapsed_time = 0.0
 static var ActivatedJezus = false
 @onready var jezus_ability_bar = $"AbilityAid/Ability 2/JesusAbilityBar/JezusAbility"
 
+#All vars for the cooldown bar , 2 diffrent ones jezus cooldown is double the time of the normal one
+#normal cooldown 3 sec
+#jezus cooldown  6 sec
+static var MaxNormalAbilityCooldown = 3.0
+static var MaxAbilityCooldown = 6.0
+static var timerAbilityCooldown = 0.0
+var timerAbility_elapsed_time = 0.0
+static var ActivatedAbility = false
+@onready var cooldown_bar = $"Ability Cooldown/CooldownBar"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rect_ability_1.visible = false
 	rect_ability_2.visible = false
 	rect_ability_3.visible = false
+	AnAbilityGotActivated()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#JezusBar
 	if ActivatedJezus:
 		timerJezusAbility -= delta
 		JezusAbility_elapsed_time += delta
 		jezus_ability_bar.size.x = 65 * (1 - JezusAbility_elapsed_time / MaxtimeJezusAbility)
-		print(timerJezusAbility)
-		
 	if timerJezusAbility < 0:
 		ActivatedJezus = false
 		jezus_ability_bar.size.x = 65
 		JezusAbility_elapsed_time = 0
 		timerJezusAbility = MaxtimeJezusAbility
+	#Ability Selector
 	if SelectedAbility != null:
 		if SelectedAbility == 1:
 			rect_ability_1.visible = true
@@ -46,10 +59,32 @@ func _process(delta):
 			rect_ability_2.visible = false
 			rect_ability_3.visible = true
 	ChangeVisibilityJesusBar()
+	#Cooldown Bar
+	if ActivatedAbility:
+		timerAbilityCooldown -= delta
+		timerAbility_elapsed_time += delta
+		if ActivatedJezus:
+			cooldown_bar.size.y = 1150 * (1 - timerAbility_elapsed_time / MaxAbilityCooldown)
+		else:
+			cooldown_bar.size.y = 1150 * (1 - timerAbility_elapsed_time / MaxNormalAbilityCooldown)
+	if timerJezusAbility < 0:
+		ActivatedAbility = false
+		timerAbility_elapsed_time = 0
+		cooldown_bar.size.x = 1150
+		
+		
+		
 static func SetAbility(nr: int):
 	SelectedAbility = nr
 static func SetActivatedJezusTrue():
 	ActivatedJezus = true
+static func AnAbilityGotActivated():
+	ActivatedAbility = true
+	#fixing timer
+	timerAbilityCooldown = MaxNormalAbilityCooldown
+	if ActivatedJezus:
+		timerAbilityCooldown = MaxAbilityCooldown
+
 func ChangeVisibilityJesusBar():
 	
 	var jezus_ability_grayed_out = $"AbilityAid/Ability 2/JesusAbilityBar/JezusAbilityGrayed out"
